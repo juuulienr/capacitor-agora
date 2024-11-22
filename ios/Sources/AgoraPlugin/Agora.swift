9c1ea67c48d9af5c549f55f92e23aa22c26702b9
Julien REIGNIER <julienreignierr@gmail.com>
2024-11-22 09:59:31 +0100

    ajout fonction initialize dans ios

--- ios/Sources/AgoraPlugin/Agora.swift
+++ ios/Sources/AgoraPlugin/Agora.swift
@@ -1,8 +1,26 @@
 import Foundation
+import AgoraRtcKit
 
 @objc public class Agora: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+  private var rtcEngine: AgoraRtcEngineKit?
+
+  @objc public func echo(_ value: String) -> String {
+    print(value)
+    return value
+  }
+
+  @objc public func initialize(appId: String) -> String {
+    if appId.isEmpty {
+      return "Invalid App ID"
     }
+
+    rtcEngine = AgoraRtcEngineKit.sharedEngine(withAppId: appId, delegate: nil)
+    if rtcEngine != nil {
+      print("rtcEngine initialized")
+      return "Agora initialized successfully"
+    } else {
+      print("rtcEngine initialization failed")
+      return "Agora initialization failed"
+    }
+  }
 }
--- ios/Sources/AgoraPlugin/AgoraPlugin.swift
+++ ios/Sources/AgoraPlugin/AgoraPlugin.swift
@@ -1,23 +1,35 @@
 import Foundation
 import Capacitor
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
 @objc(AgoraPlugin)
 public class AgoraPlugin: CAPPlugin, CAPBridgedPlugin {
-    public let identifier = "AgoraPlugin"
-    public let jsName = "Agora"
-    public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise)
-    ]
-    private let implementation = Agora()
+  public let identifier = "AgoraPlugin"
+  public let jsName = "Agora"
+  public let pluginMethods: [CAPPluginMethod] = [
+    CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise),
+    CAPPluginMethod(name: "initialize", returnType: CAPPluginReturnPromise)
+  ]
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+  private let implementation = Agora()
+
+  @objc func echo(_ call: CAPPluginCall) {
+    let value = call.getString("value") ?? ""
+    call.resolve([
+      "value": implementation.echo(value)
+    ])
+  }
+
+  @objc func initialize(_ call: CAPPluginCall) {
+    guard let appId = call.getString("appId") else {
+      call.reject("Invalid App ID")
+      return
     }
+
+    let result = implementation.initialize(appId: appId)
+    if result.contains("successfully") {
+      call.resolve(["message": result])
+    } else {
+      call.reject(result)
+    }
+  }
 }
