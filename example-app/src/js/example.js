52ee0bc7be6a6eeaf2277f761172504a8c66fe8c
Julien REIGNIER <julienreignierr@gmail.com>
2024-12-10 12:01:26 +0100

    wip

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { Agora } from '@swipelive&#x2F;capacitor-agora';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    Agora.echo({ value: inputValue })
-}
